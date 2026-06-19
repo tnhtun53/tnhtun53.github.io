390d95ab72434b106a036fc074cdf9509807cd19
tnh53 <tnh.dsa53@gmail.com>
2026-06-19 09:10:56 +0630

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showPage(pageId) {
-    const pages = document.querySelectorAll('.page');
-    pages.forEach(p => p.style.display = 'none');
-    document.getElementById(pageId).style.display = 'block';
-}
